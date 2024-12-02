e9acb70587de3a1b9b3abe9942c8f36b0cc642a1
dyw770 <dyw770@qq.com>
2024-12-02 17:20:15 +0800

    修改MonographRepository实体类型设置错误

--- admin-server/src/main/java/cn/dyw/notesnook/repository/notesnook/MonographRepository.java
+++ admin-server/src/main/java/cn/dyw/notesnook/repository/notesnook/MonographRepository.java
@@ -1,12 +1,12 @@
 package cn.dyw.notesnook.repository.notesnook;
 
-import cn.dyw.notesnook.entity.Attachment;
+import cn.dyw.notesnook.entity.Monograph;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 /**
  * @author dyw770
  * @date 2024-11-27
  */
-public interface MonographRepository extends MongoRepository<Attachment, String> {
+public interface MonographRepository extends MongoRepository<Monograph, String> {
 
 }
